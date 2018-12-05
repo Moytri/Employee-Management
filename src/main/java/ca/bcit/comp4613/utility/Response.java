a60f44c5013fcea15559d6ea4a5b5c5297cd5709
moytri <moytritopa@gmail.com>
2018-12-04 17:23:43 -0800

    Response as an Object

---
+++ src/main/java/ca/bcit/comp4613/utility/Response.java
@@ -0,0 +1,25 @@
+package ca.bcit.comp4613.utility;
+
+public class Response {
+	String code;
+	String message;
+	
+	public Response(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
